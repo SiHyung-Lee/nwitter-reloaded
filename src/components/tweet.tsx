97350607777f6101c188139748d408af8bd7317f
dev-athome <l_sh@athomecorp.com>
2024-09-19 21:47:38 +0900

    Add tweet editing functionality
    Integrated a text area for tweet editing with state management. Created new cancel and confirm buttons to handle tweet editing actions, updating the tweet content in the database.

--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -3,6 +3,7 @@ import { ITweet } from "./timeline";
 import { auth, db, storage } from "../firebase";
 import { deleteDoc, doc, updateDoc } from "firebase/firestore";
 import { deleteObject, ref } from "firebase/storage";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   display: grid;
@@ -25,8 +26,39 @@ const Photo = styled.img`
 `;
 
 const Username = styled.span`
+  display: block;
   font-weight: 600;
   font-size: 15px;
+`;
+const TextArea = styled.textarea`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 5px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  font-family:
+      system-ui,
+      -apple-system,
+      BlinkMacSystemFont,
+      "Segoe UI",
+      Roboto,
+      Oxygen,
+      Ubuntu,
+      Cantarell,
+      "Open Sans",
+      "Helvetica Neue",
+      sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
 `;
 
 const Payload = styled.p`
@@ -59,9 +91,36 @@ const EditButton = styled.button`
   border-radius: 5px;
   cursor: pointer;
 `;
+const CancelButton = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+const ConfirmButton = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
 
 export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
+  const [isEditing, setEditing] = useState(false);
+  const [editTweet, setEditTweet] = useState("");
   const user = auth.currentUser;
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setEditTweet(e.target.value);
+  };
   const onDelete = async () => {
     const ok = confirm("Are you sure you want to delete this tweet?");
     if (!ok || user?.uid !== userId) return;
@@ -77,33 +136,51 @@ export default function Tweet({ username, photo, tweet, userId, id }: ITweet) {
       //
     }
   };
-  const onEdit = async () => {
-    const ok = confirm("Are you sure you want to Edit this tweet?");
-    if (!ok || user?.uid !== userId) return;
+  const onEdit = () => setEditing(true);
+  const onConfirm = async () => {
     try {
-      await updateDoc(doc(db, "tweets", id), { capital: true });
-      /*if (photo) {
-        const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
-        await deleteObject(photoRef);
-      }*/
+      await updateDoc(doc(db, "tweets", id), { tweet: editTweet });
     } catch (e) {
       console.log(e);
     } finally {
-      console.log("aaaaa");
+      setEditing(false);
     }
+  };
+  const onCancel = () => {
+    setEditing(false);
   };
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
-        <Payload>{tweet}</Payload>
+        {isEditing ? (
+          <TextArea
+            required
+            rows={5}
+            maxLength={180}
+            onChange={onChange}
+            value={editTweet}
+          />
+        ) : (
+          <Payload>{tweet}</Payload>
+        )}
         {user?.uid === userId ? (
-          <Buttons>
-            <DeleteButton onClick={onDelete}>Delete</DeleteButton>
-            <EditButton onClick={onEdit}>Edit</EditButton>
-          </Buttons>
+          <>
+            {isEditing ? (
+              <Buttons>
+                <CancelButton onClick={onCancel}>Cancel</CancelButton>
+                <ConfirmButton onClick={onConfirm}>Confirm</ConfirmButton>
+              </Buttons>
+            ) : (
+              <Buttons>
+                <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+                <EditButton onClick={onEdit}>Edit</EditButton>
+              </Buttons>
+            )}
+          </>
         ) : null}
       </Column>
+
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
     </Wrapper>
   );
