dfa99895032acc8d30f5d4eae5c0d5f1a24c8c60
dev-athome <l_sh@athomecorp.com>
2024-09-20 16:59:44 +0900

    Enhance profile page with user avatar upload
    Added styled-components for profile page including wrappers for avatar upload and display. Integrated Firebase authentication to show the current user's display name or "Anonymous" if not available.

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,3 +1,26 @@
+import { styled } from "styled-components";
+import { auth } from "../firebase";
+
+const Wrapper = styled.div``;
+
+const AvatarUpload = styled.div``;
+
+const AvatarImg = styled.img``;
+
+const AvatarInput = styled.input``;
+
+const Name = styled.span``;
+
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const user = auth.currentUser;
+
+  return (
+    <Wrapper>
+      <AvatarUpload>
+        <AvatarImg />
+      </AvatarUpload>
+      <AvatarInput type="file" accept="image/*" />
+      <Name>{user?.displayName ? user.displayName : "Anonymous"}</Name>
+    </Wrapper>
+  );
 }
